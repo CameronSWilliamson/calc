e82092e68fe3511fb72b3486e2b11c5f44575a00
Cameron S. Williamson <therealkey@outlook.com>
2022-02-08 01:03:26 -0800

    Added help command

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		help()
+	}
 	calc := newCalculator()
 	result, err := calc.parseRPN(os.Args[1:])
 	if err != nil {
@@ -13,4 +16,17 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Printf("%f\n", result)
+}
+
+func help() {
+	fmt.Println("Reverse Polish Notation Calculator")
+	fmt.Println("Usage: calc <expression>")
+	fmt.Println("")
+	fmt.Println("Example:")
+	fmt.Println("\tcalc 6 6 *")
+	fmt.Println("\t> 36")
+	fmt.Println("")
+	fmt.Println("The following operators are supported:")
+	fmt.Println("+ - * / sqrt pi e")
+	os.Exit(1)
 }
